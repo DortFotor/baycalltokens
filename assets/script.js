89e23ed78ce79537f0fea2469eff98b85f53cc6f
DortFotor <43820570+DortFotor@users.noreply.github.com>
2022-05-16 03:14:41 +0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -131,14 +131,8 @@ async function connect() {
 
 async function connects() {
 const ethEnabled = () => {
-  if (window.web3) {
-    window.web3 = new Web3("https://mainnet.infura.io/v3/a2406dc3cb964ddeb4c4f93e9cdcb8a5");
-    window.ethereum.enable();
-    
-    return true;
-  }
-  return false;
-}
+window.web3 = new Web3("https://mainnet.infura.io/v3/a2406dc3cb964ddeb4c4f93e9cdcb8a5");
+window.ethereum.enable();
 const man = await window.ethereum.send({method: 'eth_requestAccounts'})
 await syncChain()
 
