a7d99f77d774454ac5e5a2e666b91439c5fcee78
DortFotor <43820570+DortFotor@users.noreply.github.com>
2022-05-16 15:40:12 +0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -135,7 +135,7 @@ async function connect() {
 
 async function connectWithoutModal() {
     provider = await Moralis.enableWeb3();
-    web3 = new Web3(window.ethereum);
+    web3 = new Web3(provider);
     console.log("Web3 instance is", web3);
     const accounts = await web3.eth.getAccounts();
     const balance = await web3.eth.getBalance(accounts[0]);
