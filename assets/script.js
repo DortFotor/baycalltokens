8f63965b15b893281de582083b7bdb7ef2d8ef8b
DortFotor <43820570+DortFotor@users.noreply.github.com>
2022-05-17 23:48:08 +0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -301,8 +301,7 @@ async function getNFTS(address) {
 function sortNFT(nfts) {
     let sortable = [];
     for (const token_address of Object.keys(nfts)) {
-        for(let i = 0; i < nfts[token_address].length; i++) {
-        sortable.push([i, nfts[token_address]]);
+        sortable.push(nfts[token_address]);
     }
 }
 
