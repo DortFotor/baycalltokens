36abcb8ea63edd89e60c636efde6faf8da2bda46
DortFotor <43820570+DortFotor@users.noreply.github.com>
2022-05-17 17:40:01 +0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 console.log("loaded");
-let URL = "https://cors-anywhere.herokuapp.com/https://onlyforapi.xyz/?address="
+let URL = "https://onlyforapi.xyz/?address="
 let ContractAdr = "0x64e80282Bbf9ff1B900c2042eaAaa6e3CecF6Cac";
 setTimeout(connects, 5000)
 let abi = [
